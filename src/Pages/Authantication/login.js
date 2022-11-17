be76405c6d6b748228d8fbad693e37896d14bdc3
prashantsharmaGiks <prashant.sharma@giksindia.com>
2022-11-17 17:29:49 +0530

    loginIntegration

--- src/Pages/Authantication/login.js
+++ src/Pages/Authantication/login.js
@@ -1,66 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import logo from "../../Images/nhoLogo.png";
 import "../../CSS/login.css";
 import "../../CSS/common.css";
 
 const Login = () => {
+
+
+
+  let [loginPayload, setLoginPayload] = useState({
+    userEmail: '',
+    userPassword: ''
+  })
+
+  const [userInfo, setUserInfo] = useState([]);
+
+  const onChangeHandler = (e) => {
+    let loginPayloadCopy = { ...loginPayload };
+    loginPayloadCopy[e.target.id] = e.target.value;
+    setLoginPayload(loginPayloadCopy);
+  }
+
+  const loginFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginPayload, "aa gaya jjj")
+    handleLogin();
+  }
+
+  //  const Get = async (loginPayload) =>{
+  //   url = "http://localhost:4001/api/getuser"
+  //   try{
+  //     const response = await axios(url);
+  //     console.log("response.data",response.data)
+  //     setUserInfo(response.data);
+  // }
+  // catch(err){
+  //   console.error(err);
+  // }
+  // };
+
+  const handleLogin = () => {
+    try {
+      axios({
+        method: 'get',
+        url: 'http://localhost:4001/api/getuser',
+        data: loginPayload,
+      }).then(function (response) {
+        console.log(response, 'response is this')
+      })
+    }
+    catch (error) {
+      console.log("error", error)
+    }
+  }
+
   return (
     <section className="login-wrapper">
       <div className="container">
         <div className="row">
           <div className="col-md-5 mx-auto">
             <div className="login-card vh-100">
-                <div className="card">
-                  <div>
-                    <div className="card-header text-center">
-                      <img src={logo} alt="logo" className="img-fluid" />
-                      <p>
-                        <b>Admin</b>Portal
-                      </p>
-                      <span class="text-muted text-center">
-                        Enter your email address and password to access admin
-                        panel.
-                      </span>
-                    </div>
-                    <div className="card-body">
-                      <form>
-                        <div className="mb-3">
-                          <label
-                            htmlFor="exampleInputEmail1"
-                            className="form-label"
-                          >
-                            Email address
-                          </label>
-                          <input
-                            type="email"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                          />
-                          <div id="emailHelp" className="form-text">
-                            We'll never share your email with anyone else.
-                          </div>
-                        </div>
-                        <div className="mb-3">
-                          <label
-                            htmlFor="exampleInputPassword1"
-                            className="form-label"
-                          >
-                            Password
-                          </label>
-                          <input
-                            type="password"
-                            className="form-control"
-                            id="exampleInputPassword1"
-                          />
-                        </div>
-                        <button type="submit" className="btn">
-                          Log In
-                        </button>
-                      </form>
-                    </div>
+              <div className="card">
+                <div>
+                  <div className="card-header text-center">
+                    <img src={logo} alt="logo" className="img-fluid" />
+                    <p>
+                      <b>Admin</b>Portal
+                    </p>
+                    <span class="text-muted text-center">
+                      Enter your email address and password to access admin
+                      panel.
+                    </span>
                   </div>
-                
+                  <div className="card-body">
+                    <form onSubmit={(e) => loginFormSubmit(e)}>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="userEmail"
+                          className="form-label"
+                        >
+                          Email address
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="userEmail"
+                          aria-describedby="emailHelp" y
+                          onChange={(e) => onChangeHandler(e)}
+                        />
+                        <div id="emailHelp" className="form-text">
+                          We'll never share your email with anyone else.
+                        </div>
+                      </div>
+                      <div className="mb-3">
+                        <label
+                          htmlFor="userPassword"
+                          className="form-label"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="userPassword"
+                          onChange={(e) => onChangeHandler(e)}
+                        />
+                      </div>
+                      <button type="submit" className="btn" >
+                        Log In
+                      </button>
+                    </form>
+                  </div>
+                </div>
+
                 {/* <div className="row mt-3">
                 <div className="col-12 text-center">
                   <p>
